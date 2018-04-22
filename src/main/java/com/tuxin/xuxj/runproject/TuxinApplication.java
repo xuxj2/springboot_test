3f02544bb82acd0d1bfb562bdf50998a11483991
xuxj <865136816@qq.com>
2018-04-22 22:06:11 +0800

    eee

--- src/main/java/com/tuxin/xuxj/runproject/TuxinApplication.java
+++ src/main/java/com/tuxin/xuxj/runproject/TuxinApplication.java
@@ -3,10 +3,12 @@ package com.tuxin.xuxj.runproject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //@SpringBootApplication
 @SpringBootApplication(scanBasePackages = {"com.tuxin.xuxj"}, exclude = {})
 @MapperScan("com.tuxin.xuxj.dao")
+@EnableTransactionManagement
 public class TuxinApplication {
 
 	public static void main(String[] args) {
